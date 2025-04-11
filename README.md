b63505fdc73f18108fd08c3f8af5a970768dadd8
git config boomer <timothyranow@gmail.com>
2025-04-11 03:26:50 -0600

    automated

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Clean-Your-Damn-Cell
+adapted from abandoned mod [Prisoners Are Not Swines (1.4)](https://steamcommunity.com/sharedfiles/filedetails/?id=2613966365)
---
+++ Source/CleanYourDamnCell/CleanYourDamnCell.cs
@@ -0,0 +1,61 @@
+﻿using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace CleanYourDamnCell
+{
+    public class JobGiver_CleanCell : ThinkNode_JobGiver
+    {
+        protected override Job TryGiveJob(Pawn pawn)
+        {
+            Room room = pawn.GetRoom();
+            if (room == null || !room.IsPrisonCell)
+            {
+                return null;
+            }
+            Map map = pawn.Map;
+            Job job = JobMaker.MakeJob(JobDefOf.Clean);
+            foreach (IntVec3 cell in room.Cells)
+            {
+                foreach (Thing thing in cell.GetThingList(map))
+                {
+                    if (HasJobOnThing(pawn, thing))
+                    {
+                        job.AddQueuedTarget(TargetIndex.A, thing);
+                    }
+                }
+                if (job.GetTargetQueue(TargetIndex.A).Count >= 15)
+                {
+                    break;
+                }
+            }
+            if (job.targetQueueA == null || job.targetQueueA.Count < 1)
+            {
+                return null;
+            }
+            job.targetQueueA.SortBy((LocalTargetInfo targ) => targ.Cell.DistanceToSquared(pawn.Position));
+            return job;
+        }
+
+        private bool HasJobOnThing(Pawn pawn, Thing t)
+        {
+            if (!(t is Filth filth))
+            {
+                return false;
+            }
+            if (!filth.Map.areaManager.Home[filth.Position])
+            {
+                return false;
+            }
+            if (!pawn.CanReserve(t))
+            {
+                return false;
+            }
+            if (filth.TicksSinceThickened < 600)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
